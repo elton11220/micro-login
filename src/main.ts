de774f37aa2a57ca6b2576ba6ae156e46b6d0996
elton11220 <jonesbak@outlook.com>
2022-09-18 11:48:00 +0800

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,50 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import "./public-path";
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import App from "./App.vue";
+import { routes } from "@/router";
 
-createApp(App).use(router).mount('#app')
+let router = null;
+// @ts-ignore
+let app = null;
+let history = null;
+function render(props = {}) {
+  // @ts-ignore
+  const { container } = props;
+  history = createWebHistory(window.__POWERED_BY_QIANKUN__ ? "/login" : "/");
+  router = createRouter({
+    history,
+    routes,
+  });
+  app = createApp(App)
+    .use(router)
+    .mount(container ? container.querySelector("#app") : "#app");
+}
+
+if (!window.__POWERED_BY_QIANKUN__) {
+  if (process.env.NODE_ENV === "development") {
+    render();
+    console.warn(
+      "\n您正在development环境中运行该应用\n以production环境运行时，将不允许脱离qiankun微前端基座访问该应用"
+    );
+  } else {
+    console.error("不允许脱离qiankun微前端基座访问该应用，请在qiankun微前端的主应用中访问");
+  }
+}
+
+export async function bootstrap() {
+  console.log("微应用加载");
+}
+
+export async function mount(props: any) {
+  console.log("props", props);
+  render(props);
+}
+
+export async function unmount() {
+  // @ts-ignore
+  app.unmount();
+  history = null;
+  app = null;
+  router = null;
+}
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
+const { name } = require("./package");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  devServer: {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    port: 9000,
+  },
+  configureWebpack: {
+    output: {
+      library: name,
+      libraryTarget: "umd",
+    },
+  },
+});
