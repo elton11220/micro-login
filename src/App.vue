7aead6641e455c87fcd5de888526e6d47499f39c
elton11220 <jonesbak@outlook.com>
2022-09-19 22:42:57 +0800

    feat(micro-app): 在应用挂载时为全局共享状态添加观察者，并将全局状态使用provide注入到应用

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,42 @@
   </NMessageProvider>
 </template>
 
-<script setup>
+<script lang="ts" setup>
 import { NMessageProvider } from "naive-ui";
+import { microAppStateActions } from "./microAppStateActions";
+import { onMounted, provide, ref } from "vue";
+
+const globalState = ref({
+  appName: "",
+  appId: "",
+  appDescription: "",
+});
+
+provide("globalState", globalState);
+
+onMounted(() => {
+  microAppStateActions.onGlobalStateChange((state, prev) => {
+    if (
+      state.appName &&
+      (state.appName !== prev.appName || !globalState.value.appName)
+    ) {
+      globalState.value.appName = state.appName;
+    }
+    if (
+      state.appId &&
+      (state.appId !== prev.appId || !globalState.value.appId)
+    ) {
+      globalState.value.appId = state.appId;
+    }
+    if (
+      state.appDescription &&
+      (state.appDescription !== prev.appDescription ||
+        !globalState.value.appDescription)
+    ) {
+      globalState.value.appDescription = state.appDescription;
+    }
+  }, true);
+});
 </script>
 
 <style lang="scss">
